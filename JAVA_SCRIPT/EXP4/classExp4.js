f9e13e5252cb9167697713deec97ece098ee6886
deepak-0912 <deepak.kumar@loconav.com>
2019-06-27 18:06:07 +0530

    exercise4

---
+++ JAVA_SCRIPT/EXP4/classExp4.js
@@ -0,0 +1,23 @@
+class Form {
+  constructor() {
+    this.numid = document.getElementById("numId");
+    this.resid = document.getElementById("resId");
+    this.subid = document.getElementById("subId");
+  }
+
+  myFunction(e) {
+    // eslint-disable-next-line no-restricted-globals
+    if (isNaN(this.numid.value)) {
+      this.resid.value = "false";
+      e.preventDefault();
+    } else {
+      this.resid.value = "true";
+    }
+  }
+
+  display() {
+    this.subid.addEventListener("click", this.myFunction.bind(this));
+  }
+}
+const object1 = new Form();
+object1.display();
